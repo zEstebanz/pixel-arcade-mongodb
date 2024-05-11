44acf6baf33c58c65c75138045cc180379b827a0
oller.esteban <estebanoller73@gmail.com>
2024-05-11 17:17:23 -0300

    fuente pixel

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,5 @@ export { default } from "next-auth/middleware";
 //configuracion, donde tiene un valor que contiene las rutas protegidas.
 // ":path*" (proteje todas las rutas que esten dentro de ...)
 export const config = {
-    matcher: ['/dashboard/:path*', '/games/:path*', '/juego/:path*','/turnos/:path*']
+    matcher: ['/dashboard/:path*', '/turnos/:path*']
 }
